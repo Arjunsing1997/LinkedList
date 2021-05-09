4a4887352e1f402f6ecb434e0fd5abe5c85f42bc
Arjunsing1997 <acchur1997@gmail.com>
2021-05-09 16:42:46 +0530

    UC3 Appnding

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -13,13 +13,21 @@ namespace LinkedList
             Console.WriteLine("Welcome to LINKED LIST");
 
             LinkedList list = new LinkedList();
+            //Inserted node at the end
             //list.InsertLast(56);
             //list.InsertLast(30);
             //list.InsertLast(70);
 
-            list.InsertFront(70); // Adding data in list //UC2
-            list.InsertFront(30);// Adding data in list //UC2
-            list.InsertFront(56);// Adding data in list //UC2
+            //Inserting node at the beginning
+            //list.InsertFront(70); // Adding data in list //UC2
+            //list.InsertFront(30);// Adding data in list //UC2
+            //list.InsertFront(56);// Adding data in list //UC2
+
+            //Appending Operation
+            list.Appending(56);// Adding data in list //UC3
+            list.Appending(30);// Adding data in list //UC3
+            list.Appending(70);// Adding data in list //UC3
+            
             list.Display();
             Console.ReadLine();
         }
